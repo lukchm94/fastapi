b373991dd1ceea7201437063182b42b85dda1318
Lukasz Chmielewski <lukchm94@gmail.com>
2023-09-23 09:11:39 +0200

    updated endpoints

--- FastAPI-The-Complete-Course-main/fastapi/src/main.py
+++ FastAPI-The-Complete-Course-main/fastapi/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -16,3 +16,48 @@ BOOKS = [
 @app.get("/books")
 async def first_api():
     return {"message": BOOKS}
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return: list[str] = []
+    for book in BOOKS:
+        if book.get("category").casefold().lower() == category.casefold().lower():
+            books_to_return.append(book)
+
+    if len(books_to_return) > 0:
+        return {"message": books_to_return}
+
+    else:
+        return {"message": f"no books from {category.casefold().upper()}"}
+
+
+@app.get("/books/{dynamic_param}")
+async def read_all_books(dynamic_param: str):
+    for book in BOOKS:
+        if book.get("title").lower() == dynamic_param.casefold():
+            return {"message": book}
+        else:
+            return {"message": "book not found"}
+
+
+@app.get("/book/{title}")
+async def get_book_by_title(title: str):
+    print(title.casefold().lower())
+    if title.casefold().lower() in [book["title"].lower() for book in BOOKS]:
+        return {
+            "message": [
+                book
+                for book in BOOKS
+                if str(book["title"].lower()) == title.casefold().lower()
+            ]
+        }
+    else:
+        return {"message": "book not found"}
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    # new_book = Body()
+    print(new_book)
+    BOOKS.append(new_book)
