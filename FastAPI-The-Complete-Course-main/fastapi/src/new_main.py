45a9e3b27f13c9ee2f16b454df03bb8c05753508
Lukasz Chmielewski <lukchm94@gmail.com>
2023-09-27 10:47:22 +0200

    completed mods for year_published

--- FastAPI-The-Complete-Course-main/fastapi/src/new_main.py
+++ FastAPI-The-Complete-Course-main/fastapi/src/new_main.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from exceptions import RatingError, YearError
 from models.books import BOOKS, Book, BookRequest, get_new_id
 
 from fastapi import FastAPI
@@ -6,8 +9,71 @@ app = FastAPI()
 
 
 @app.get("/books")
-async def first_api():
-    return {"message": BOOKS}
+async def get_all_books():
+    return {"results": len(BOOKS), "message": BOOKS}
+
+
+@app.get("/books/{book_id}")
+async def get_book_by_id(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return {"message": book}
+    return {"message": f"No book found for ID: {book_id}"}
+
+
+@app.get("/books/published_after/{year}")
+async def get_book_published_after(year: int):
+    if year > datetime.now().year or year < 2000:
+        raise YearError(year=year)
+
+    books_to_return: list[Book] = []
+    for book in BOOKS:
+        if book.year_published >= year:
+            books_to_return.append(book)
+    if len(books_to_return) > 0:
+        return {"results": len(books_to_return), "data": books_to_return}
+    else:
+        return {"message": f"No book published after {year}"}
+
+
+@app.get("/books/published_before/{year}")
+async def get_book_published_before(year: int):
+    if year > datetime.now().year or year < 2000:
+        raise YearError(year=year)
+
+    books_to_return: list[Book] = []
+    for book in BOOKS:
+        if book.year_published <= year:
+            books_to_return.append(book)
+    if len(books_to_return) > 0:
+        return {"results": len(books_to_return), "data": books_to_return}
+    else:
+        return {"message": f"No book published after {year}"}
+
+
+@app.put("/books/update_year_published/{year}")
+async def updated_year_published(book: BookRequest):
+    if book.year_published > datetime.now().year or book.year_published < 2000:
+        raise YearError(year=book.year_published)
+
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+@app.get("/books/")
+async def get_book_by_rating(rating: int):
+    if rating > 10:
+        raise RatingError(rating=rating)
+
+    books_to_return: list[Book] = []
+    for book in BOOKS:
+        if book.rating > rating:
+            books_to_return.append(book)
+    if len(books_to_return) > 0:
+        return {"message": books_to_return}
+    else:
+        return {"message": f"No book found with rating higher than {rating}"}
 
 
 @app.post("/create_book/")
@@ -15,3 +81,18 @@ async def create_book(book_request: BookRequest):
     book_request.id = get_new_id(books=BOOKS)
     new_book: Book = Book(**book_request.dict())
     BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
