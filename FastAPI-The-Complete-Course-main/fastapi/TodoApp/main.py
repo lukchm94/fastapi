6e8171ef0126e43d5de75add838c1925ae37689e
Lukasz Chmielewski <49422763+lukchm94@users.noreply.github.com>
2023-09-30 16:41:01 +0200

    run first API with local DB (#9)

---
+++ FastAPI-The-Complete-Course-main/fastapi/TodoApp/main.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+
+from database import SessionLocal, engine
+from models import Base, Todos
+from sqlalchemy.orm import Session
+
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+DB_DEPENDENCY = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/todos")
+async def read_all(db: DB_DEPENDENCY):
+    return db.query(Todos).all()
