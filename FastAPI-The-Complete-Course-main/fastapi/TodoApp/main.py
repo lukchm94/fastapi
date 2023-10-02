80f071eec23ee06dd4b62efd1f39c1c16db4514e
Lukasz Chmielewski <49422763+lukchm94@users.noreply.github.com>
2023-10-02 09:59:21 +0200

    Project todos (#10)
    * run first API with local DB
    
    * added comment for virtual env
    
    * added CRUD to local DB

--- FastAPI-The-Complete-Course-main/fastapi/TodoApp/main.py
+++ FastAPI-The-Complete-Course-main/fastapi/TodoApp/main.py
@@ -1,10 +1,13 @@
+# source fastapienv/bin/activate
 from typing import Annotated
 
 from database import SessionLocal, engine
-from models import Base, Todos
+from exceptions import NoDataException
+from model import Base, Todos
+from models.todos import TodoRequest
 from sqlalchemy.orm import Session
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Path, status
 
 app = FastAPI()
 
@@ -22,6 +25,49 @@ def get_db():
 DB_DEPENDENCY = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/todos")
+@app.get("/home", status_code=status.HTTP_200_OK)
 async def read_all(db: DB_DEPENDENCY):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo_by_id(db: DB_DEPENDENCY, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return {"data": todo_model}
+    raise NoDataException(todo_id=todo_id)
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: DB_DEPENDENCY, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(
+    db: DB_DEPENDENCY, todo_request: TodoRequest, todo_id: int = Path(gt=0)
+):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise NoDataException(todo_id=todo_id)
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: DB_DEPENDENCY, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise NoDataException(todo_id=todo_id)
+
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
