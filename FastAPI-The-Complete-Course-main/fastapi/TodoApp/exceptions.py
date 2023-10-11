c04088fcfef370b61a99872958aa9e3d34e98ba2
Lukasz Chmielewski <lukchm94@gmail.com>
2023-10-11 10:04:50 +0200

    new branch

--- FastAPI-The-Complete-Course-main/fastapi/TodoApp/exceptions.py
+++ FastAPI-The-Complete-Course-main/fastapi/TodoApp/exceptions.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from fastapi import HTTPException
 
@@ -7,8 +7,29 @@ class NoDataException(HTTPException):
     def __init__(
         self,
         todo_id: int,
-        status_code: int = 404,
-        detail: str = "TODO with id {todo_id} not found",
+        status_code: Optional[int] = 404,
+        detail: Optional[str] = "TODO with id {todo_id} not found",
         headers: Dict[str, Any] | None = None,
     ) -> None:
         super().__init__(status_code, detail.format(todo_id=todo_id), headers)
+
+
+class MissingUserException(HTTPException):
+    def __init__(
+        self,
+        username: str,
+        status_code: Optional[int] = 401,
+        detail: Optional[str] = "User: {username} not found",
+        headers: Dict[str, Any] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail.format(username=username), headers)
+
+
+class FailedAuthenticationException(HTTPException):
+    def __init__(
+        self,
+        status_code: Optional[int] = 401,
+        detail: Optional[str] = "Authentication failed. Wrong password provided",
+        headers: Dict[str, Any] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
